712829a5d507248a14c07e3c4bc9bc67396c87a8
Jacopown <jacopomehi@gmail.com>
2021-11-13 15:02:09 +0100

    initialized Weapon.h

--- Model/Sword.h
+++ Model/Sword.h
@@ -1,15 +1,11 @@
-/**
- * Project Untitled
- */
-
-
 #ifndef _SWORD_H
 #define _SWORD_H
 
 #include "Weapon.h"
-
-
-class Sword: public Weapon {
+class Sword : public Weapon
+{
+    explicit Sword(int damage = 1, int level = 1); //TODO choose default damage value
+    ~Sword();
 };
 
 #endif //_SWORD_H
