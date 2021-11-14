dd3f0fcf4567acaa30bebad57e331692c9f19ad9
Jacopown <jacopomehi@gmail.com>
2021-11-14 18:13:27 +0100

    started implementation of map.h and .cpp

--- Model/Map.cpp
+++ Model/Map.cpp
@@ -1,19 +1,19 @@
-/**
- * Project Untitled
- */
-
-
 #include "Map.h"
+#include <iostream>
 
-/**
- * Map implementation
- */
-
-
-void Map::spawnEntities() {
-
+void Map::spawnEntities(const std::vector<MapElements> &mapElementsVector)
+{
+    for (int i = 0; i < mapElementsVector.size(); i++)
+    //TODO add function to spawn elements
 }
 
-void Map::spawnBonfire() {
-
+Map::Map(const std::vector<MapElements> &mapElementsVector, int dimX, int dimY)
+{
+    this->dimX = dimX;
+    this->dimY = dimY;
+    for (int i = 0; i < mapElementsVector.size(); i++)
+    {
+        this->mapElementsVector[i] = mapElementsVector[i];
+    }
 }
+Map::~Map() {}
--- Model/Map.h
+++ Model/Map.h
@@ -1,21 +1,19 @@
-/**
- * Project Untitled
- */
-
-
 #ifndef _MAP_H
 #define _MAP_H
+#include "MapElements.h"
+#include <vector>
 
-class Map {
-public: 
-    
-void spawnEntities();
-    
-void spawnBonfire();
-protected: 
-    int[2] dim;
-    int mapHeight;
-    MapElements& mapElementList[];
+class Map
+{
+public:
+    void spawnEntities(const std::vector<MapElements> &mapElementsVector);
+
+protected:
+    Map(const std::vector<MapElements> &mapElementsVector, int dimX, int dimY);
+    ~Map();
+    int dimX;
+    int dimY;
+    std::vector<MapElements> mapElementsVector;
 };
 
 #endif //_MAP_H
