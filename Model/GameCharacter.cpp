62e31267668db575e0a71abe4c89cd11daca8662
andrea <andrea.assioma@stud.unifi.it>
2021-11-17 22:28:57 +0100

    removed textPool attribute and isDialogue method. Note for Hero class: needs to be added these two things

--- Model/GameCharacter.cpp
+++ Model/GameCharacter.cpp
@@ -1,7 +1,7 @@
 #include "GameCharacter.h"
 
-GameCharacter::GameCharacter(int hp, int a, int c, int mS, std::string& t): HP(hp), armor(a), cash(c), movementSpeed(mS),
-weapon(nullptr), leftWeapon(nullptr), textPool(t){
+GameCharacter::GameCharacter(int hp, int a, int c, int mS): HP(hp), armor(a), cash(c), movementSpeed(mS),
+weapon(nullptr), leftWeapon(nullptr){
 }
 GameCharacter::GameCharacter(const sf::Vector2f& pos,float rectPosX, float rectPosY, float rectWidth, float rectHeight): 
                         pos(pos), rectPosX(rectPosX), rectPosY(rectPosY), rectWidth(rectWidth), rectHeight(rectHeight){
@@ -111,78 +111,4 @@ bool GameCharacter::isChasing(int aggroDistance, const GameCharacter &enemy) {//
     if (sf::norm(pos-enemy.pos) > aggroDistance) //is it ok?
         return false;
     return true;
-}
-
-
-//for npcs and bosses voicelines
-void GameCharacter::isDialogue() {
-    std::cout << textPool[dialogueTracker] << std::endl;
-    //at the bottom
-    if (dialogueTracker < textPool.size())
-        dialogueTracker ++;
-    //TODO add dialogues in main in a array of arrays
-    //TODO add extra dialogues after beating a boss
-    /*NPC
-    Elizabeth
-    "Bene bene, abbiamo un nuovo ospite. Da quale lontana era provieni?"
-    "Ci sono molte cose che vorrei chiederti, ma so che non devo."
-    "Ahh, capisco. Non sai cosa sia successo..."
-    "Vedrai più avanti. Un antica bestia è stata risvegliata
-     e ha generato l’Abisso, minacciando di corrompere tutta Oolacile."
-    "Il cavaliere Artorias è venuto per fermare tutto questo,
-     ma un tale eroe non è abbastanza coraggioso da affrontare tale potere."
-    "Senza dubbio è stato sconfitto, travolto dall’oscurità,
-     ma spero ancora che la Principessa Dusk venga salvata."
-    "Non molto tempo fa, ho avuto un altro visitatore, un essere umano come te,
-     da un tempo lontano."
-    "Solo che era terribilmente odioso… e tempo che sia ancora tra noi."
-    "Indossava un cappello e un lungo cappotto nero…"
-    "Che le fiamme possano guidarti."
-    "Ti ringrazio per aver salvato la principessa Dusk, te ne sarò debitrice e
-     ti ricorderò per sempre."
-    "Ma terrò per me la tua storia perché una leggenda rimarrà sempre una leggenda…"
-    //Sif
-    "(Ulula)"
-    //Chester
-    "Oh… Fammi indovinare"
-    "Sei stato catapultato nel passato di un’era differente?"
-    "Come avevo sospettato. È successo anche a me: siamo entrambi degli estranea
-     in una terra sconosciuta."
-    "Ma almeno adesso, siamo in due."
-    "Beh… Errore mio."
-    "Ma siamo entrambi avventurieri e, per questo, dobbiamo aiutarci a vicenda."
-    "Ti è capitato di incontrare Artorias? Il leggendario camminatore degli Abissi,
-     secondo gli antichi racconti."
-    "Se ancora non è successo, meglio così. Se me lo stai chiedendo, sì, è ormai corrotto."
-    "Ahahah ah ahah !"
-    "Allora, cosa ti ha fare quel fungo gigante? Non che mi interessi.
-     Non fa parte del mio business."
-    "Eheheheh  eheh…"
-    "Hm? Non ho molto da dire…"
-    "Hai veramente ucciso Artorias?"
-    "Ho sentito dire che l’Abisso lo ha trasformato in qualcosa di veramente pericoloso."
-    "È assolutamente insidioso."
-    "Ahahahahaha ahha!"
-    "Che tu ci creda o no, Oolacile ha portato l’Abisso con se."
-    "Ti sei mai chiesto: Vale veramente la pena?"
-    "Eheheheh eheh eh…"
-    //Principessa Dusk
-    "...Mmn...ahh…"
-    "…Aah...mmn…"
-    "Salve avventuriero. Anche tu da queste parti, non è vero? Da quale era provieni?"
-    "Sai… può sembrare strano, ma…"
-    "Sono stata assalita da una creatura dell’Abisso e ci sarei
-     morta se non fosse stato per il Grande Cavaliere Artorias."
-    "In realtà, ho visto poco di quanto è accaduto durante lo scontro. Ero stordita."
-    "Nonostante ciò, Artorias emanava un’aura particolare… In lui traspirava
-     un equilibrio interiore…"
-    "Che ritrovo tale e quale in te."
-    "..."
-    //Artorias
-    "Chiunque tu sia, stai lontano!"
-    "Presto... Sarò consumato... da loro, dall'Oscurità"
-    "REEEUUUUGHHHHHHHH!"; //deve essere ripetuta anche dopo "ti prego, la..."
-    "Ti prego, la propagazione dell'Abisso... deve essere fermata"
-    //Manus
-    "Restituiscimi ciò che mi appartiene!"*/
 }
