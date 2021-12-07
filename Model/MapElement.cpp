2ba1a0cbd83cf77ea4c1c3f021b08981d1c4bea3
Jacopown <jacopomehi@gmail.com>
2021-12-07 11:59:43 +0100

    added spriteDimension attribute

--- Model/MapElement.cpp
+++ Model/MapElement.cpp
@@ -1,9 +1,10 @@
-#include "MapElement.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <string>
 #include <vector>
 #include <iostream>
+
+#include "MapElement.h"
 
 void MapElement::draw(sf::RenderWindow &window)
 {
@@ -20,14 +21,18 @@ void MapElement::setPos(sf::Vector2f &newPos)
     mapElementSprite.setPosition(newPos);
 }
 
-MapElement::MapElement(std::vector<int> &spriteDimension, sf::Vector2f &position, std::string &mapElementFilePath)
+MapElement::MapElement(std::vector<int> &spriteDimension, sf::Vector2f &position, const std::string &mapElementFilePath)
 {
+    this->spriteDimension.left = spriteDimension[0];
+    this->spriteDimension.top = spriteDimension[1];
+    this->spriteDimension.width = spriteDimension[2];
+    this->spriteDimension.height = spriteDimension[3];
     if (!mapElementTexture.loadFromFile(mapElementFilePath))
     {
         std::cout << "Error while setting MapElement texture" << std::endl;
     }
     mapElementSprite.setTexture(this->mapElementTexture);
-    mapElementSprite.setTextureRect(sf::IntRect(spriteDimension[0], spriteDimension[1], spriteDimension[2], spriteDimension[3]));
+    mapElementSprite.setTextureRect(MapElement::spriteDimension);
     mapElementSprite.setPosition(position);
 }
 MapElement::~MapElement() {}
