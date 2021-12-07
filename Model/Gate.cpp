a18f12617c8e65be547d3dad50af54324173b396
Jacopown <jacopomehi@gmail.com>
2021-12-07 11:56:40 +0100

    graphic implementation of opened Gate

--- Model/Gate.cpp
+++ Model/Gate.cpp
@@ -1,5 +1,7 @@
 #include "Gate.h"
+
 #include <SFML/System.hpp>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -8,11 +10,28 @@ bool Gate::getisOpen()
     return isOpen;
 }
 
-void Gate::setisOpen(bool isOpen) //TODO implement graphic change for open or close
+void Gate::setisOpen(bool isOpen)
 {
     this->isOpen = isOpen;
+    if (isOpen)
+    {
+        spriteDimension.left = spriteDimension.left + spriteDimension.width;
+    }
+    else
+    {
+        spriteDimension.left = spriteDimension.left - spriteDimension.width;
+    }
+    mapElementSprite.setTextureRect(spriteDimension);
 }
 
-Gate::Gate(std::vector<int> &spriteDimension, sf::Vector2f &position, std::string &mapElementFilePath, bool isOpen) : MapElement(spriteDimension, position, mapElementFilePath), isOpen(isOpen) {}
+Gate::Gate(std::vector<int> spriteDimension, sf::Vector2f position, const std::string mapElementFilePath, bool isOpen) : MapElement(spriteDimension, position, mapElementFilePath), isOpen(isOpen)
+{
+    if (isOpen)
+    {
+        this->spriteDimension.left = this->spriteDimension.left + this->spriteDimension.width;
+    }
+    mapElementSprite.setTextureRect(this->spriteDimension);
+    mapElementSprite.setPosition(position);
+}
 
 Gate::~Gate() {}
--- Model/Gate.h
+++ Model/Gate.h
@@ -1,10 +1,11 @@
 #ifndef _GATE_H
 #define _GATE_H
 
-#include "MapElement.h"
 #include <SFML/System.hpp>
 #include <string>
 #include <vector>
+
+#include "MapElement.h"
 
 class Gate : public MapElement
 {
@@ -13,7 +14,7 @@ public:
 
     void setisOpen(bool isOpen);
 
-    Gate(std::vector<int> &spriteDimension, sf::Vector2f &position, std::string &mapElementFilePath, bool isOpen = false);
+    Gate(std::vector<int> spriteDimension, sf::Vector2f position, const std::string mapElementFilePath, bool isOpen = false);
 
     ~Gate();
 
