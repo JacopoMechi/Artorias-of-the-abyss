07d98bf61f3dc95619fcb7401474c0a1cb952aaa
Jacopown <jacopomehi@gmail.com>
2021-12-07 16:56:31 +0100

    firse bonfire graphic implementation

--- Model/Bonfire.cpp
+++ Model/Bonfire.cpp
@@ -1,19 +1,25 @@
-#include "Bonfire.h"
 #include <SFML/System.hpp>
 #include <string>
 #include <vector>
 
-void Bonfire::resetHp() //TODO to be implemented
+#include "Bonfire.h"
+
+void Bonfire::draw(sf::RenderWindow &window)
+{
+    window.draw(mapElementSprite);
+}
+
+void Bonfire::resetHp() // TODO to be implemented
 {
 }
 
-void Bonfire::setSpawnPoint() //TODO to be implemented
+void Bonfire::setSpawnPoint() // TODO to be implemented
 {
 }
 
-void Bonfire::respawnEnemies() //TODO to be implemented
+void Bonfire::respawnEnemies() // TODO to be implemented
 {
 }
 
-Bonfire::Bonfire(std::vector<int> &spriteDimension, sf::Vector2f &position, std::string &mapElementFilePath) : MapElement(spriteDimension, position, mapElementFilePath) {}
+Bonfire::Bonfire(std::vector<int> spriteDimension, sf::Vector2f position, const std::string mapElementFilePath) : MapElement(spriteDimension, position, mapElementFilePath) {}
 Bonfire::~Bonfire() {}
--- Model/Bonfire.h
+++ Model/Bonfire.h
@@ -1,21 +1,24 @@
 #ifndef _BONFIRE_H
 #define _BONFIRE_H
 
-#include "MapElement.h"
 #include <SFML/System.hpp>
 #include <string>
 #include <vector>
 
+#include "MapElement.h"
+
 class Bonfire : public MapElement
 {
 public:
-    void resetHp(); //TODO to be implemented
+    void draw(sf::RenderWindow &window);
 
-    void setSpawnPoint(); //TODO to be implemented
+    void resetHp(); // TODO to be implemented
 
-    void respawnEnemies(); //TODO to be implemented
+    void setSpawnPoint(); // TODO to be implemented
 
-    Bonfire(std::vector<int> &spriteDimension, sf::Vector2f &position, std::string &mapElementFilePath);
+    void respawnEnemies(); // TODO to be implemented
+
+    Bonfire(std::vector<int> spriteDimension, sf::Vector2f position, std::string mapElementFilePath);
 
     ~Bonfire();
 };
