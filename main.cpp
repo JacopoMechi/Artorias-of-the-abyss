dd8ee3245589c94ba844de4fe288b9159278f59f
Jacopown <jacopomehi@gmail.com>
2021-12-07 12:00:22 +0100

    added gates spawn

--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 #include <string>
 
 #include "GameCharacter.h"
+#include "Gate.h"
 #include "Map.h"
 
 #define MAPPATH "../Textures/Lvl1.png"
+#define TEXTURESPATH "../Textures/Textures.png"
 
 int main()
 {
-    // initializin objects
-    Map map({}, MAPPATH);
+    // defining sprite options
+    std::vector<int> gateDimension = {0, 0, 123, 194};
+    std::vector<int> mirroredGateDimension = {123, 0, -123, 194};
+    sf::Vector2f leftGatePosition(150.f, 450.f);
+    sf ::Vector2f rightGatePosition(1630.f, 450.f);
+    // initializing objects
+    Gate leftGate(gateDimension, leftGatePosition, TEXTURESPATH, false);
+    Gate rightGate(mirroredGateDimension, rightGatePosition, TEXTURESPATH, false);
+    rightGate.setisOpen(true);
+    std::vector<MapElement> porte = {leftGate, rightGate};
+    Map map(porte, MAPPATH);
 
     // create window
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Artoryas of the abyss");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Hallway of the abyss");
 
     // starting the game loop
     while (window.isOpen())
