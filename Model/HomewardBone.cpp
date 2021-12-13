eb0c263a729dd34cd0ff6d2b81e429b26fd67bdb
andrea <andrea.assioma@stud.unifi.it>
2021-12-13 20:11:05 +0100

    adjusted HomewardBone constructor

--- Model/HomewardBone.cpp
+++ Model/HomewardBone.cpp
@@ -1,6 +1,8 @@
 #include "HomewardBone.h"
 
-HomewardBone::HomewardBone(std::string n, int p, std::string d, int M, int c): Item(n, p, d, M, c){
+HomewardBone::HomewardBone(std::string itemName, int itemPrice, std::string itemDescription, int maxItemCount, int itemCount,
+                           int x, int y, int width, int height): Item(itemName, itemPrice, itemDescription, maxItemCount, itemCount,
+                           x, y, width, height){
 
 }
 
@@ -8,7 +10,7 @@ HomewardBone::~HomewardBone(){
 
 }
 
-void HomewardBone::use(Hero& hero, MapElements& bonfire) {
+/*void HomewardBone::use(Hero& hero, MapElements& bonfire) {
     //if interaction
     hero.respawn(bonfire.getPosX()-2, bonfire.getPosY());
-}
+}*/
