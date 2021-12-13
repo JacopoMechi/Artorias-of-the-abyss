789891423b42ad871c7226651250d34e0f39ca92
andrea <andrea.assioma@stud.unifi.it>
2021-12-14 00:10:18 +0100

    updated Pendant constructor

--- Model/Pendant.h
+++ Model/Pendant.h
@@ -2,12 +2,13 @@
 #define _PENDANT_H
 
 #include "Item.h"
-#include "GameCharacter.h"
+//#include "GameCharacter.h"
 
 class Pendant: public Item {
 public://is it correct the override like this?
-    explicit Pendant(std::string n = "Pendente", int p = 10000,
-                     std::string d = "Permette la deviazione delle spells", int M = 100000, int c = 0);
+    explicit Pendant(std::string itemName = "Pendente", int itemPrice = 10000,
+                     std::string itemDescription = "Permette la deviazione delle spells", int maxItemCount = 100000, int itemCount = 0,
+                     int x = 497, int y = 98, int width = 26, int height = 21);
     ~Pendant();
     void use(GameCharacter& hero);
 private:
