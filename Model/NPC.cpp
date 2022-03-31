c207750bdc282f077d68ae7e078d61cf9cdf3659
Jacopown <jacopomehi@gmail.com>
2022-03-31 11:02:51 +0200

    implemented NPC class

--- Model/NPC.cpp
+++ Model/NPC.cpp
@@ -1,15 +1,195 @@
-/**
- * Project Untitled
- */
-
-
 #include "NPC.h"
 
-/**
- * NPC implementation
- */
+NPC::NPC(sf::RenderWindow &window, int type, const sf::Vector2f& pos, int hp, int armor, int cash, float movementSpeed) : 
+    window(window), type(type), GameCharacter(pos, hp, armor, cash, movementSpeed){
+        //loading npcs sprites' rectangles
+        //chester
+        if(type == 0){
+            sprite.setTextureRect({394, 22, 16, 14});
 
+            //setting up items for chester
+            //homeward bones
+            merch[1] -> setItemCount(50);
 
-void NPC::sellItems() {
+            //chester's dialogue
+            textPool.resize(sizeof(chesterPool)/sizeof(std::string));
+            textPool.insert(textPool.begin(), &chesterPool[0], &chesterPool[sizeof(chesterPool)/sizeof(std::string)]);
+        //elizabeth
+        }else if(type == 1){  
+            sprite.setTextureRect({393, 40, 22, 25});
 
+            //setting up items for elizabeth
+            //pendant
+            merch[2] -> setItemCount(1);
+            //green blossom
+            merch[0] -> setItemCount(50);
+            
+            //elizabeth's dialogue
+            textPool.resize(sizeof(elizabethPool)/sizeof(std::string));
+            textPool.insert(textPool.begin(), &elizabethPool[0], &elizabethPool[(sizeof(elizabethPool)/sizeof(std::string))]);
+        //dusk    
+        }else if(type == 2){
+            sprite.setTextureRect({422, 35, 23, 30});
+
+            //dusk's dialogue
+            textPool.resize(sizeof(duskPool)/sizeof(std::string));
+            textPool.insert(textPool.begin(), &duskPool[0], &duskPool[sizeof(duskPool)/sizeof(std::string)]);
+        //sif
+        }else if(type == 3){
+            sprite.setTextureRect({452, 38, 25, 27});    
+        }
+        //loading shop sprite
+        hudTexture.loadFromFile("../Textures/PlayerHUD.png");
+        shopSprite.setTexture(hudTexture);
+        shopSprite.setTextureRect({91, 357, 317, 312});
+        shopSprite.setPosition(730, 303);
+        shopSprite.setScale(1.5f, 1.5f);
+
+        //interaction box sprite
+        interactionBoxSprite.setTexture(hudTexture);
+        interactionBoxSprite.setTextureRect({993, 317, 289, 96});
+
+        //text for ineteracting box
+        interactFont.loadFromFile("../orangekid.ttf");
+        interactText.setFont(interactFont);
+
+        //tracker for buying a specific item
+        trackerSprite.setTexture(hudTexture);
+        trackerSprite.setTextureRect({118, 738, 264, 55});
+        trackerSprite.setScale(1.5f, 1.5f);
+        trackerSprite.setColor({255,255, 255, 80});
+}
+
+NPC::~NPC(){
+    delete *merch;
+}
+
+void NPC::interact(Hero &hero) {
+    //checking if player is close enough to npc and
+    //if true, it will pop-up a box like "Press to interact"
+    //and it will be possible to press the interaction button
+    if(this -> isAggro(190, hero)){
+        if(!isInteraction){
+           //reset interact box to default
+           interactionBoxSprite.setScale(1.0f, 1.0f);
+           this -> drawInteractBox({815,862});//for the interaction box trigger
+           this -> drawText("Premi Q per interagire", {855, 889});//for displaying message
+        }
+        aggro = true;   
+    }else
+        aggro = false;
+
+    //handling npc interaction menu 
+    if(isInteraction){
+        if(!isShop && !isTalking){
+            //reset interact box to default
+            interactionBoxSprite.setScale(1.0f, 1.0f);
+
+            //displaying interaction box
+            this -> drawInteractBox({810, 303});
+
+            //diplaying text for interaction box
+            if(type == 0 || type == 1){//for chester and elizabeth
+                this -> drawText("[1] Parla       [2] Acquista\n[Q] Esci",{825, 305});
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+                    isShop = !isShop;
+            }else //for the other npcs
+                this -> drawText("[1] Parla       [Q] Esci",{825, 305});
+
+            //selcting between previous choises
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+                isTalking = !isTalking;    
+
+        //setting up npc shop    
+        }else if(isShop){
+            //reset npc dialouge tracker
+            dialogueTracker = 0;
+
+            //drawing shop sprite
+            window.draw(shopSprite);
+            //chester which sells homeward bones
+            if(type == 0){
+
+                //drawing tracker for scrolling items in the shop
+                this -> drawTracker(trackerPos);
+
+                this -> drawShop(merch[1]);
+            //eliszabeth which sells green blossoms and the pendant
+            }else if (type == 1){
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                    trackerPos = {773, 340};
+                else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                    trackerPos = {773, 445};   
+                this -> drawTracker(trackerPos);
+                this -> drawShop(merch[0], merch[2]);
+            }
+        //starting dialogue with npc    
+        }else if(isTalking){
+            //upscaling interact box
+            interactionBoxSprite.setScale(1.7f, 1.7f);
+            //showing npc's dialogue box
+            this -> drawInteractBox({800, 303});
+            //showing dialouge
+            if(type == 3)//only Sif has one line of dialogue
+                this -> drawText(sifPool, {815, 313});
+            else
+                this -> drawText(textPool[dialogueTracker], {815, 313});
+        }else{
+
+            //reset npc dialouge tracker
+            dialogueTracker = 0;
+            
+            //reset items highlights for shop
+            trackerPos = {773, 340};
+        }
+    }
+}
+
+void NPC::updateInputs(sf::Event keyInput){
+    if(aggro){
+        if(keyInput.type == sf::Event::KeyPressed && keyInput.key.code == sf::Keyboard::Q){
+            isInteraction = !isInteraction;//open/close shop
+            //for resetting interaction
+            isShop = false;
+            isTalking = false;
+        }
+    }
+
+    if(isTalking){//TODO LATER adjust dialogue depending on situations (like changing chester's text pool when Artorias is killed)
+        //scrolling through character's phrases
+        if(keyInput.type == sf::Event::KeyPressed && keyInput.key.code == sf::Keyboard::Enter)
+            dialogueTracker = (dialogueTracker+1)%(textPool.size()/2);//FIXME why /2?
+    }
+}
+
+void NPC::drawText(std::string text, sf::Vector2f textPos){
+    interactText.setPosition(textPos);
+    interactText.setString(text);
+    window.draw(interactText);
+}
+
+void NPC::drawInteractBox(sf::Vector2f pos){
+    interactionBoxSprite.setPosition(pos);
+    window.draw(interactionBoxSprite);
+}
+
+void NPC::drawShop(Item* item1){
+    item1 -> displayItem(780, 355, window);
+    item1 -> displayName(window, 900, 360);
+}
+
+void NPC::drawShop(Item* item1, Item* item2){
+    item1 -> displayItem(785, 358, window);
+    item1 -> displayName(window, 900, 360);
+    item2 -> displayItem(780, 463, window);
+    item2 -> displayName(window, 900, 470);
+}
+
+void NPC::drawTracker(sf::Vector2f pos){
+    trackerSprite.setPosition(pos);
+    window.draw(trackerSprite);
+}
+
+bool NPC::getIsInteraction(){
+    return isInteraction;
 }
--- Model/NPC.h
+++ Model/NPC.h
@@ -1,18 +1,150 @@
-/**
- * Project Untitled
- */
-
-
 #ifndef _NPC_H
 #define _NPC_H
 
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+#include <memory>
+
 #include "GameCharacter.h"
+#include "Item.h"
+#include "Hero.h"
+#include "HomewardBone.h"
+#include "Pendant.h"
+#include "GreenBlossom.h"
 
 
 class NPC: public GameCharacter {
+
 public:
 
-void sellItems();
+    NPC(sf::RenderWindow &window, int type, const sf::Vector2f& pos, int hp = 100, int armor = 0, int cash = 0, float movementSpeed = 0);
+    ~NPC();
+
+    void receiveDamage() = delete;//to be unkillable
+
+    void interact(Hero &hero);
+
+    //handling inputs
+    void updateInputs(sf::Event keyInput);
+
+    //drawing text for all menus 
+    void drawText(std::string text, sf::Vector2f textPos);
+
+    //drawing interaction box
+    void drawInteractBox(sf::Vector2f pos);
+
+    //drawing shop box
+    void drawShop(Item* item1, Item* item2);
+    void drawShop(Item* item1);
+
+    //drawing tracker for selecting an item to buy
+    void drawTracker(sf::Vector2f pos);
+
+    //to stop character moving while interacting with an NPC
+    bool getIsInteraction();
+
+private:
+
+    //getting window from main
+    sf::RenderWindow &window;
+
+    //loading hud textures
+    sf::Texture hudTexture;
+
+    //shop sprite declaration
+    sf::Sprite shopSprite;
+
+    //interaction sprite declaration
+    sf::Sprite interactionBoxSprite;
+
+    //text for "press Q to interact" box 
+    sf::Text interactText;
+    sf::Font interactFont;
+
+    //tracker sprite
+    sf::Sprite trackerSprite;
+
+    //initial tracker pos
+    sf::Vector2f trackerPos = {773, 340};
+
+    //switch for open shop
+    bool isInteraction = false;
+
+    //switch for interaction menu
+    bool aggro = false;
+
+    //switch for shop menu
+    bool isShop = false;
+
+    //switch for talking
+    bool isTalking = false;
+
+    //for tracking character type
+    int type;
+
+    //for tracking phrase number
+    int dialogueTracker = 0;
+
+    //items selling list
+    Item* merch[3] = {new GreenBlossom(), new HomewardBone(), new Pendant()};//TODO correct array declaration in other classes
+
+    //setting textpool for dialogues depending on character
+    std::vector<std::string> textPool;
+
+    //inserting dialogue strings
+    std::string elizabethPool[12] = {"Bene bene, abbiamo un nuovo ospite. \nDa quale lontana era provieni?",//Elizabeth
+                            "Ci sono molte cose che vorrei chiederti, \nma so che non devo.",
+                            "Ahh, capisco. Non sai cosa sia successo...",
+                            "Vedrai più avanti. Un antica bestia è \nstata risvegliata "
+                            "e ha generato l'Abisso, \nminacciando di corrompere tutta Oolacile.",
+                            "Il cavaliere Artorias è venuto per fermare \ntutto questo, "
+                            "ma un tale eroe non è \nabbastanza coraggioso da affrontare \ntale potere.",
+                            "Senza dubbio è stato sconfitto, travolto \ndall'oscurità, "
+                            "ma spero ancora \nche la Principessa Dusk venga salvata.",
+                            "Non molto tempo fa, ho avuto un altro \nvisitatore, un essere umano come te, \n"
+                            "da un tempo lontano.",
+                            "Solo che era terribilmente odioso… e \ntemo che sia ancora tra noi.",
+                            "E' calvo e indossa un indumento marrone...",
+                            "Che le fiamme possano guidarti.",
+                            "Ti ringrazio per aver salvato la \nprincipessa Dusk, te ne sarò debitrice \ne "
+                            "ti ricorderò per sempre.",
+                            "Ma terrò per me la tua storia \nperché una leggenda rimarrà sempre \nuna leggenda…"
+                            };
+    std::string sifPool = {"(Ulula)"};//sif
+    std::string chesterPool[19] = {"Oh… Fammi indovinare",//chester
+                            "Sei stato catapultato nel passato di un'era \ndifferente?",
+                            "Come avevo sospettato. È successo anche a \nme: siamo entrambi degli estranei \n"
+                            "in una terra sconosciuta.",
+                            "Ma almeno adesso, siamo in due.",
+                            "Beh… Errore mio.",
+                            "Ma siamo entrambi avventurieri e, per questo, \ndobbiamo aiutarci a vicenda.",
+                            "Ti è capitato di incontrare Artorias? Il \nleggendario camminatore degli Abissi, \n"
+                            "secondo gli antichi racconti.",
+                            "Se ancora non è successo, meglio così. Se \nme lo stai chiedendo, sì, è ormai corrotto.",
+                            "Ahahah ah ahah !",
+                            "Allora, cosa ti ha fare quel fungo gigante? Non \nche mi interessi. "
+                            "Non fa parte del mio business.",
+                            "Eheheheh  eheh...",
+                            "Hm? Non ho molto da dire...",
+                            "Hai veramente ucciso Artorias?",
+                            "Ho sentito dire che l'Abisso lo ha trasformato in \nqualcosa di veramente pericoloso.",
+                            "È assolutamente insidioso.",
+                            "Ahahahahaha ahha!",
+                            "Che tu ci creda o no, Oolacile ha portato \nl'Abisso con se.",
+                            "Ti sei mai chiesto: Vale veramente la pena?",
+                            "Eheheheh eheh eh..."};
+    std::string duskPool[9] = {"...Mmn...ahh...",//Princess Dusk
+                            "...Aah...mmn...",
+                            "Salve avventuriero. Anche tu da queste parti, \nnon è vero? Da quale era provieni?",
+                            "Sai... può sembrare strano, ma...",
+                            "Sono stata assalita da una creatura dell'Abisso \ne ci sarei "
+                            "morta se non fosse stato per \nil Grande Cavaliere Artorias.",
+                            "In realtà, ho visto poco di quanto è accaduto \ndurante lo scontro. Ero stordita.",
+                            "Nonostante ciò, Artorias emanava un'aura \nparticolare... In lui traspirava "
+                            "un equilibrio \ninteriore...",
+                            "Che ritrovo tale e quale in te.",
+                            "..."};
 };
 
-#endif //_NPC_H
+#endif
