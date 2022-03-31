5ff4eca3fe2243d6a28fe0960af3d6ada8ef2b77
Jacopown <jacopomehi@gmail.com>
2022-03-31 11:06:54 +0200

    moved method implementationin hero from gamecharacter

--- Model/Hero.cpp
+++ Model/Hero.cpp
@@ -1,28 +1,14 @@
 #include "Hero.h"
 
-Hero::Hero(int hp, int a, int c, int s, std::string& t, int d, int cd, float hPosX, float hPosY):
-GameCharacter(hp, a, c, s, t), dashCount(d), cooldown(cd), heroPosX(hPosX), heroPosY(hPosY){
-
+Hero::Hero(bool isKnight, const sf::Vector2f& pos, int hp, int armor, int cash, float movementSpeed):
+    isKnight(isKnight), GameCharacter(pos, hp, armor, cash, movementSpeed){
+    //TODO switch between mage and knight by checking isKnight
 }
 
-Hero::~Hero(){
-
-}
-
-float Hero::getHeroPosX(){
-    return heroPosX;
-}
-void Hero::setHeroPosX(float heroPosX){
-    this -> heroPosX = posX;
-}
-
-float Hero::getHeroPosY(){
-    return heroPosY;
-}
-
-void Hero::setHeroPosY(float heroPosY) {
-    this -> heroPosY = posY;
-}
+//Hero::~Hero(){//TODO
+    //if (leftWeapon != nullptr)FIXME
+    //    delete leftWeapon;
+//}
 
 int Hero::getDash(){
     return dashCount;
@@ -32,29 +18,23 @@ void Hero::setDash(int dashCount) {
 }
 
 int Hero::getCooldown(){
-    return cooldown;
-}
-void Hero::setCooldown(int cooldown) {
-    this -> cooldown = cooldown;
+    return dashTimeHolding;
 }
 
-void Hero::dash(int dashCount, int cooldown){
-    //SFML if key is pressed reduce dashCount. When it reaches 0, start the cooldown
+void Hero::setCooldown(float dashTimeHolding){
+    this -> dashTimeHolding = dashTimeHolding;
 }
 
-void Hero::restoreHp(int amount){
-    GameCharacter::setHp(GameCharacter::getHp+amount);
+void Hero::dash(){
+    //for dashing, we just need to move the character position farther only in the moment that we press Space key
+    dashing = dashDistance*dir;
+    if(dashCount > 0){
+        dashCount --;
+        pos += dashing; 
+    }
 }
 
-void Hero::addItem() {
-
-}
-
-void Hero::useItem() {
-
-}
-
-void Hero::switchZone() {
+void Hero::switchZone() {//TODO useful later maybe
 
 }
 
@@ -62,30 +42,82 @@ void Hero::useBonfire() {
 
 }
 
-/**
- * @param bool isKnight
- */
-void Hero::raiseShield(void bool isKnight) {
+
+void Hero::raiseShield() {
 
 }
 
-/**
- * @param bool isMage
- * @param int cooldown
- * @return override
- */
-override Hero::attack(void bool isMage, void int cooldown) {
-    return null;
+
+void Hero::attack() {
+    if(isKnight){
+        //create texture rect and set nFrames
+        if(dir.x > 0.0f){
+            frameRect = {0, 158, 31, 22};
+            lastActionFrame = frameRect;
+        }else if(dir.x < 0.0f){    
+            frameRect = {31, 158, -31, 22};
+            lastActionFrame = frameRect;
+        }else
+            frameRect = lastActionFrame;
+    }else{
+        //same here but for the mage
+    }
+    actionStarting = true;
+    iFrame = 0;
 }
 
-/**
- * @return override
- */
-override Hero::movement() {
-    return null;
+//handling character action inputs like attack, roll, interact
+void Hero::updateDelayAndInputs(sf::Event keyInput, float dt) {
+    //updating delay time
+    delayTime = dt;
+    //for attacking
+    if(keyInput.type == sf::Event::KeyPressed && keyInput.key.code == sf::Keyboard::F)
+        this -> attack();
+    //for dashes
+    if(keyInput.type == sf::Event::KeyPressed && keyInput.key.code == sf::Keyboard::Space)
+        this -> dash();
+    //for using items in quickslot
+    //for dash cooldown
+    
+    //handling dash cooldowns
+    if(dashCount < maxDashes){
+        dashTime += dt;
+        if(dashTime >= dashTimeHolding){
+            dashTime = 0;
+            dashCount++;
+        }
+    }
+}
+void Hero::movement(bool isInventoryOpen, bool isInteracting){
+    if (!isInventoryOpen && !isInteracting && !actionStarting){
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+            dir.y = -1.0f;
+        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+            dir.y = 1.0f;
+        }else
+        dir.y = 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+            dir.x = -1.0f;
+        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+            dir.x = 1.0f;
+        }else
+            dir.x = 0;
+
+    }else
+        dir = {0,0};
 }
 
 
 void Hero::respawn(float posX,float posY) {
     //finish hero
+}
+
+Weapon* Hero::getShield() {
+    return leftWeapon;
+}
+
+void Hero::setShield(Weapon* leftWeapon) {
+    this->leftWeapon = leftWeapon;
 }
--- Model/Hero.h
+++ Model/Hero.h
@@ -1,66 +1,79 @@
 #ifndef _HERO_H
 #define _HERO_H
 
+#include <string>
+#include <cmath>
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
 #include "GameCharacter.h"
-#include "Inventory.h"
 #include "Bonfire.h"
 
 
 
 class Hero: public GameCharacter {
+    //TODO add a method to switch hero's armor and weapon when a boss is killed
 public: 
 
-    Hero(int hp, int a, int c, int s, std::string& t, int d, int cd, float hPosX, float hPosY);
-                                                    //d: dashCount, heroPosXY: heroPosXposY
+    Hero(bool isKnight, const sf::Vector2f& pos, int hp, int armor, int cash, float movementSpeed);
+    virtual ~Hero() = default;
 
     int getDash();
     void setDash(int dashCount);
 
+    //for green blossom which reduce dashes cooldown
     int getCooldown();
-    void setCooldown(int cooldown);
-
-    float getHeroPosX();
-    void setHeroPosX(float heroPosX);
+    void setCooldown(float dashTimeHolding);
 
     float getHeroPosY();
     void setHeroPosY(float heroPosY);
 
-    void dash(int dashCount, int cooldown);
+    void dash();
 
     void addItem();
-    
-    void useItem();
     
     void switchZone();
     
     void useBonfire();
 
-    void restoreHp(int amount);
+    void raiseShield();
     
-/**
- * @param bool isKnight
- */
-    void raiseShield(void bool isKnight);
+    void attack() override;
+
+    void updateDelayAndInputs(sf::Event keyInput, float dt);
     
-/**
- * @param bool isMage
- * @param int cooldown
- */
-    override attack(void bool isMage, void int cooldown);
-    
-    override movement();
+    void movement(bool isInventoryOpen, bool isInteracting);
     
     void respawn(float posX, float posY);
+
+    Weapon* getShield();
+    void setShield(Weapon* weapon);
+
 protected: 
-    Inventory& inv;
-    bool isMage;
+
+    int dialogueTracker = 0;
+    bool isInventoryOpen;
+    Weapon* leftWeapon;
     bool isKnight;
-    int cooldown;
+    float delayTime;
+
+    //handling dashes
+    int dashCount = 3;
+    int maxDashes = 3;
+    float dashTimeHolding = 1.0f;
+    float dashTime = 0.0f;
+ 
+
+    float actionTime = 0.0f;
+    int actionFrame = 0;
+    sf::IntRect lastActionFrame = {0, 158, 31, 22};
+
+    //int cooldown = 5;
     Bonfire* bonfire ;
     int amount;
-    int dashCount;
-    float heroPosX;
-    float heroPosY;
+
+    sf::Vector2f dashing;
+    float dashDistance = 100.0f;
 };
 
 #endif //_HERO_H
