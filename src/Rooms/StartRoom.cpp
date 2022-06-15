8c8184e1cb702ac6cb8ce9ebc249e9f69a07b24f
Jacopown <jacopomehi@gmail.com>
2022-06-15 11:23:58 +0200

    removed unused cases

--- src/Rooms/StartRoom.cpp
+++ src/Rooms/StartRoom.cpp
@@ -1,20 +1,8 @@
 #include "StartRoom.h"
 
-StartRoom::StartRoom(int level, sf::RenderWindow &window): Room(level, window){
-    switch(level){
-        case 1:
-            roomFilePath = roomPath1;
-            //TODOdraw enemies
-            break;
-        case 2:
-            roomFilePath = roomPath2;
-            //TODOdraw enemies
-            break;
-        case 3:
-            roomFilePath = roomPath2;
-            //TODOdraw enemies
-            break;
-    }
+StartRoom::StartRoom(int level, sf::RenderWindow &window) : Room(level, window)
+{
+    roomFilePath = roomPath1;
     rightGate = std::make_unique<Gate>(window, true, true);
     roomTexture.loadFromFile(roomFilePath);
     roomSprite.setTexture(roomTexture);
