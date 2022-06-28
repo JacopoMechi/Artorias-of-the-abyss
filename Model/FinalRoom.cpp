44df9f754d185d58ba9f78df2480f98fa8c203f8
andrea <andrea.assioma@stud.unifi.it>
2022-06-28 18:04:18 +0200

    finished adjusting new implementations

--- Model/FinalRoom.cpp
+++ Model/FinalRoom.cpp
@@ -4,15 +4,15 @@ FinalRoom::FinalRoom(int level, sf::RenderWindow &window): Room(level, window){
     switch(level){
         case 1:
             roomFilePath = roomPath1;
-            //TODO spawn enemies
+            //TODO spawn boss
             break;
         case 2:
             roomFilePath = roomPath2;
-            //TODO spawn enemies
+            //TODO spawn boss
             break;
         case 3:
             roomFilePath = roomPath3;
-            //TODO spawn enemies
+            //TODO spawn boss
             break;    
     }
     leftGate = std::make_unique<Gate>(window);
--- Model/MiddleRoom.cpp
+++ Model/MiddleRoom.cpp
@@ -4,21 +4,20 @@ MiddleRoom::MiddleRoom(int level, sf::RenderWindow &window): Room(level, window)
     switch(level){
         case 1:
             roomFilePath = roomPath1;
-            npc = std::unique_ptr<NPC>(new NPC(window, 0, {300, 300}, 1, 1, 0, 0));
-            //TODO draw enemies
+            npc = std::unique_ptr<NPC>(new NPC(window, 1, {300, 300}, 1, 1, 0, 0));//Elizabeth
             break;
         case 2:
             roomFilePath = roomPath2;
-            //TODO draw enemies
+            npc = std::unique_ptr<NPC>(new NPC(window, 0, {300, 300}, 1, 1, 0, 0));//Chester
             break;
         case 3:
             roomFilePath = roomPath3;
-            //TODO draw enemies
+            npc = std::unique_ptr<NPC>(new NPC(window, 3, {300, 300}, 1, 1, 0, 0));//Sif
             break;
     }
     leftGate = std::make_unique<Gate>(window);
     rightGate = std::make_unique<Gate>(window, true, false);
-    bonfire = std::unique_ptr<Bonfire>(new Bonfire(window, {500.0f, 500.0f}));
+    bonfire = std::unique_ptr<Bonfire>(new Bonfire(window, {900.0f, 200.0f}));
     if(!roomTexture.loadFromFile(roomFilePath))
         std::cout << "Error on loading second room's texture" << std::endl;
     roomSprite.setTexture(roomTexture);
