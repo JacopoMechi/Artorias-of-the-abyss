0a728df0708e27adbedb867e81a99f8f906f0901
andrea <andrea.assioma@stud.unifi.it>
2022-07-08 13:56:52 +0200

    implemented catalyst attack animation

--- src/Catalyst.cpp
+++ src/Catalyst.cpp
@@ -11,6 +11,79 @@ void Catalyst::setLevel(int level) //TODO Choose damage for every level
         damage = 15;
 }
 
-Catalyst::Catalyst(int level) : Weapon(level) {}
+void Catalyst::use(sf::RenderWindow &window, sf::IntRect entityRect, sf::Vector2f entityPos, float dt){
+    if(startAnimation){
+        window.draw(spellSprite);
+        //setting position and rectangles of the weapon
+        if(entityRect.width > 0){
+            currentRect = weaponRect;
+            xVariation = 10;
+            yVariation = 40;
+        }else if(entityRect.width < 0){
+            currentRect = {weaponRect.width, weaponRect.top, -weaponRect.width, weaponRect.height};
+            xVariation = -20;
+            yVariation = 40;
+        }
 
-Catalyst::~Catalyst() {}
+        //updating iFrames for weapon
+        weaponAnimationTime += dt;
+        if(weaponAnimationTime >= weaponAnimationHolding){
+            iWeaponFrame = (++iWeaponFrame) % nFrames;
+            weaponAnimationTime = 0.0f;
+            if(iWeaponFrame == 0){
+                startAnimation = false;
+                startSpell = true;
+            }
+        }
+
+        //drawing animation
+        if(startAnimation){
+            weaponSprite.setPosition(entityPos.x+xVariation, entityPos.y+yVariation);
+            weaponSprite.setTextureRect({currentRect.left + iWeaponFrame*abs(currentRect.width), currentRect.top, currentRect.width, currentRect.height});
+            window.draw(weaponSprite);
+        }
+    }
+
+    if(startSpell){
+        if(0 < spellPos.x && spellPos.x < 1920){ //to set the range of the spell       
+
+            //to move the spell horizzontaly
+            spellPos.x += spellSpeed*dt*spellDirection; 
+            spellSprite.setPosition(spellPos);
+            
+            //printing on screen
+            window.draw(spellSprite);
+        }else
+            startSpell = false;
+    }
+}
+
+void Catalyst::setSpellDirection(sf::Vector2f heroPos, sf::IntRect heroRect){
+    if(heroRect.width > 0){
+        spellDirection = 1; //right
+        spellPos.x = heroPos.x + 150;
+        currentSpellRect = spellRect;
+    }if(heroRect.width < 0){
+        spellDirection = -1; //left
+        spellPos.x = heroPos.x - 150;
+        currentSpellRect = {spellRect.width, spellRect.top, -spellRect.width, spellRect.height};
+    }
+    spellPos.y = heroPos.y +40;
+    spellSprite.setTextureRect(currentSpellRect);
+    spellSprite.setPosition(spellPos);
+}
+
+bool Catalyst::getStartSpell() const{
+    return startSpell;
+}
+
+Catalyst::Catalyst(sf::IntRect weaponRect, sf::Vector2f weaponScale, int nFrames, int level) : 
+    Weapon(weaponRect, weaponScale, nFrames, level) {
+    spellSprite.setTexture(texture);
+    spellRect = {3,299, 33, 20};
+    currentSpellRect = spellRect;
+    spellSprite.setTextureRect(currentSpellRect);
+    spellSprite.setScale(3.5f, 3.5f);
+    spellPos = {650, 540};//the hero spawn facing to the right and at the position 500, 500
+    spellSprite.setPosition(spellPos);
+}
--- src/Catalyst.h
+++ src/Catalyst.h
@@ -8,10 +8,27 @@ class Catalyst : public Weapon
 public:
     virtual void setLevel(int level) override;
 
-protected:
-    Catalyst(int level = 1);
+    virtual void use(sf::RenderWindow &window, sf::IntRect entityRect, sf::Vector2f entityPos, float dt) override;
 
-    ~Catalyst();
+    void setSpellDirection(sf::Vector2f heroPos, sf::IntRect heroRect);
+
+    bool getStartSpell() const;
+
+    explicit Catalyst(sf::IntRect weaponRect = {2, 332, 38, 36}, sf::Vector2f weaponScale = {3.5f, 3.5f}, int nFrames = 1, int level = 1);
+
+    virtual ~Catalyst() = default;
+
+protected:
+    //for spell casting
+    sf::Sprite spellSprite;
+    sf::Vector2f spellPos;
+    float spellSpeed = 500;
+    //to swap spell in the direction that is casted
+    sf::IntRect spellRect;
+    sf::IntRect currentSpellRect;
+    int spellDirection = 1;//1 because the hero starts from the right
+    //to cast the spell
+    bool startSpell = false;
 };
 
 #endif //_CATALYST_H
