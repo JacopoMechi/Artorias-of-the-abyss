e1d3ca4ddc6f1e15067c323c51edfb10f4819b43
Andrea <aligiassioma72@gmail.com>
2022-07-13 16:42:39 +0200

    changed name of Fighting Fixture cpp (now is EntitiesInteractionsTest) and updated cmake

--- test/EntitiesInteractionsTest.cpp
+++ test/EntitiesInteractionsTest.cpp
@@ -8,23 +8,20 @@ TEST(Hero, TestAttack){
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "testing attack");
     Hero h(window, true, {500, 500},1 , 100, 0, 100);
     Enemy1 e(window, {730, 266}, 1, 100, 0, 100);
-    Game game(window);
-}
-
-/*class EntitiesSuite : public ::testing::Test{
-  protected:
-    virtual void setUp(){
-        //e.setPosition({});
+    h.getWeapon() -> setStartAnimation(true);
+    h.setCanAttack(false);
+    h.updateDelay(0.016725f);
+    while(h.getWeapon() -> getIFrame() < h.getWeapon() -> getNFrames()-1){
+        window.clear();
+        //for testing the attack we set delayTime of hero to 0.016725 (we suppose to get the time to execute all the functions)  
+        h.update(0.016725f);
+        e.update(0.016725f); 
+        h.draw(window);     
+        e.draw(window);
+        h.attack(window);
+        h.dealDamage(e);
+        window.display();
     }
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "attack test");  
-    Hero h(window, true, {500, 500}, 1, 100, 0, 100);
-    Enemy1 e(window, {730, 266}, 1, 100, 0, 100);
-    Game game(window);
-};
-
-TEST_F(EntitiesSuite, TestAttack){
-    h.attack(window);
-    h.dealDamage(e);
     ASSERT_EQ(5, h.getWeapon() -> getDamage());//checking if it deals the correct amount of damage
     ASSERT_EQ(-4, e.getHp());//checking if he received damage
-}*/
+}
