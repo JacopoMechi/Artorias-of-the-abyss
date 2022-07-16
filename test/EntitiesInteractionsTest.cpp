df4ca3c80f857f016f97d199089c54416388346f
Andrea <aligiassioma72@gmail.com>
2022-07-16 22:18:03 +0200

    implemented test for mages attack

--- test/EntitiesInteractionsTest.cpp
+++ test/EntitiesInteractionsTest.cpp
@@ -4,7 +4,7 @@
 #include "../src/Game.h"
 #include <SFML/Graphics.hpp>
 
-//testing if hero deals damage to enemy
+//testing if knight deals damage to enemy
 TEST(Hero, TestAttack){
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "testing attack");
     Hero h(window, true, {500, 500}, 1 , 100, 0);
@@ -25,6 +25,29 @@ TEST(Hero, TestAttack){
     }
     ASSERT_EQ(5, h.getWeapon() -> getDamage());//checking if it deals the correct amount of damage
     ASSERT_EQ(-4, e.getHp());//checking if he received damage
+}
+
+//testing if mage deals damage to enemy
+TEST(Mage, TestAttack){
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "testing attack");
+    Hero h(window, false, {500, 500}, 1 , 100, 0);
+    Enemy1 e(window, {800, 500}, 1, 0, 0, 100);
+    h.getWeapon() -> setStartAnimation(true);
+    h.setCanAttack(false);
+    h.updateDelay(0.016725f);
+    while(e.getHp() >= 0){
+        window.clear();
+        //for testing the attack we set delayTime of hero to 0.016725 (we suppose to get the time to execute all the functions)  
+        h.update(0.016725f);
+        e.update(0.016725f); 
+        h.draw(window);     
+        e.draw(window);
+        h.attack(window);
+        h.dealDamage(e);
+        window.display();
+    }
+    ASSERT_EQ(15, h.getWeapon() -> getDamage());//checking if it deals the correct amount of damage
+    ASSERT_EQ(-14, e.getHp());//checking if he received damage
 }
 
 //testing if enemy deals damage to hero
