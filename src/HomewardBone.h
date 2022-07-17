b3f617521312201a57d313211220e2cbe2e1145c
Andrea <aligiassioma72@gmail.com>
2022-07-17 07:42:57 +0200

    removed comments

--- src/HomewardBone.h
+++ src/HomewardBone.h
@@ -2,13 +2,11 @@
 #define _HOMEWARDBONE_H
 
 #include "Item.h"
-//#include "MapElements.h"
-//#include "Hero.h"
 
 class HomewardBone: public Item {
 public: 
     explicit HomewardBone(std::wstring itemName = L"Osso del Ritorno", int itemPrice = 2000,
-                 std::wstring itemDescription = L"Permette il teletrasporto \nall'ultimo falo' interagito", int maxItemCount = 3, int itemCount = 1,
+                 std::wstring itemDescription = L"Permette il teletrasporto \nall'ultimo falo' interagito", int maxItemCount = 3, int itemCount = 0,
                  sf::IntRect spriteRect = {497, 71, 23, 22}, sf::IntRect effectRect = {0, 0, 0, 0});
     virtual ~HomewardBone();
     virtual void use(Hero& hero) override;
