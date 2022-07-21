4dea1dc21d92ec9ad9621c4e8afff2f8da4f817d
Andrea <andrea@Andreas-MBP.homenet.telecomitalia.it>
2022-07-21 18:29:03 +0200

    created header to insert all constants of the project

---
+++ src/Constants.h
@@ -0,0 +1,10 @@
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+
+//defining constant values for classes
+
+//for hero's initial position
+const float heroInitPosX = 500.0f;
+const float heroInitPosY = 500.0f;
+
+#endif
